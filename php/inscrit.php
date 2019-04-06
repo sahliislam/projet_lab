2a0ae9ba5919d7866938e06b23b10a197d271614
sahli_islam <islamsahli133@gmail.com>
2019-04-06 16:39:32 +0200

    hh

--- php/inscrit.php
+++ php/inscrit.php
@@ -1,6 +1,6 @@
 <?php 
 //configuration 
-include "C:\xampp\htdocs\projet_lab\php\config1.php";
+include "../css1/";
 $base=connect(); 
 //recuperation des donnes 
 $nom=$_POST['nom']; 
@@ -8,13 +8,13 @@ $Prenom=$_REQUEST['prenom'];
 $numero=$_REQUEST['nom_du_champ']; 
 $Genre=$_REQUEST['gender']; 
 $Email=$_REQUEST['email']; 
-$Date=$_REQUEST['date-naissance'];
+$Date=$_REQUEST['date'];
 $nat=$_POST['liste'];
 $interet=$_POST['interet'];
 $MDP= md5($_REQUEST['password']);
 $CMDP=md5($_REQUEST['cpassword']);
 //insertion des donnes dans la base
-$req=" INSERT INTO insciption (nom , prenom,num,genre,mail,dates, nat ,interet, motdp , con ) VALUES ('$nom' , '$Prenom' , '$Telephone' ,'$Genre','$Email' , '$Date','$nat' ,'$interet' ,'$MDP' ,'$CMDP') ";
+$req=" INSERT INTO formalab (nom , prenom,num,genre,mail,dates, nat ,interet, motdp , con ) VALUES ('$nom' , '$Prenom' , '$Telephone' ,'$Genre','$Email' , '$Date','$nat' ,'$interet' ,'$MDP' ,'$CMDP') ";
 //exec : function insert update Delete
 //type de retour exec int || bool
 //int : nbr de lignes inserèes  || boolean : false en cas d'echec 
