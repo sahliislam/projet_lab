9465a3abfc931c53015e3eda018e31f85905a35f
sahli_islam <islamsahli133@gmail.com>
2019-04-06 17:41:29 +0200

    eee

---
+++ php/connexion.php
@@ -0,0 +1,14 @@
+    
+<?php 
+//configuration 
+$_connection=mysqli_connect("localhost","root","","isg");
+
+//recuperation des donnes 
+$email=$_REQUEST['email'];
+$pass=$_REQUEST['password'];
+$req= " SELECT * FROM insciption where ( (email='$email' ) AND (motdepasse='$pass'))";
+$result = $base->query($req);
+if ( $result <=0  ) {
+echo "introuvable";
+}
+?>
