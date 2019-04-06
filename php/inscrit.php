2cf9996453531e7c7400e2c12dbbc8c29ce69717
sahli_islam <islamsahli133@gmail.com>
2019-04-06 15:57:02 +0200

    hhhh

--- php/inscrit.php
+++ php/inscrit.php
@@ -1,4 +1,25 @@
-<?php
-    $nom=$_GET['nom'];
-    echo $nom;
-    ?>
+<?php 
+//configuration 
+include "C:\xampp\htdocs\dos\php\config.php";
+$base=connect(); 
+//recuperation des donnes 
+$nom=$_POST['Nom']; 
+$Prenom=$_REQUEST['Prenom']; 
+$numero=$_REQUEST['nom_du_champ']; 
+$Genre=$_REQUEST['genre']; 
+$Email=$_REQUEST['email']; 
+$Date=$_REQUEST['date-naissance'];
+$nat=$_POST['liste'];
+$interet=$_POST['interet'];
+$MDP= md5($_REQUEST['password']);
+$CMDP=md5($_REQUEST['cpassword']);
+//insertion des donnes dans la base
+$req=" INSERT INTO insciption (nom , prenom ,tele,genre,email,datenaiss , nationalite ,interet, motdepasse , confirmation ) VALUES ('$nom' , '$Prenom' , '$Telephone' ,'$Genre','$Email' , '$Date','$nat' ,'$interet' ,'$MDP' ,'$CMDP') ";
+//exec : function insert update Delete
+//type de retour exec int || bool
+//int : nbr de lignes inserèes  || boolean : false en cas d'echec 
+$res = $base->exec($req);
+if ($res == 1 ) {
+    echo "donnees inserees "; }
+    else {echo  " verifiez la req";}
+?>
